f9f2dc950364c0965f96992c7614e3d1e8639238
Diego Pellitero <diego.pellitero@outlook.com>
2020-04-14 22:04:52 +0200

    Final version

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -11,19 +11,19 @@ class Node:
         self.data = data
 
 
-    def PrintTree(self):
+    def PrintNode(self):
         print("\t\t",self.data, "\n", self.left, "\t\t\t", self.right)
 
 
 """
-Receives a sanitized list with the operations and builds the binary tree
+Receives a list with the operations and builds the binary tree
 """
 def create_tree(operations):
     while can_strip_parenthesis(operations):
         operations = strip_parenthesis(operations)
     index = lowest_priority_operation(operations)
     if index != -1:
-        print("En la cadena ", operations, " el indice de menos prioridad es ", index)
+        #print("En la cadena ", operations, " el indice de menos prioridad es ", index)
         if index == 0:
              root = Node(operations[index], None, None)
         else:
@@ -36,8 +36,9 @@ def create_tree(operations):
 allowed_ops = {"+": 1, "-": 1, "*": 2, "/": 2, "**":3}
 zerodivision = "div by 0"
 
-
-#Receives a sanitized list with the operations and returns the index of the lowert priority
+"""
+Receives list with the operations and returns the index of the lowert priority
+"""
 def lowest_priority_operation(_list):
     if len(_list) > 1:
         
@@ -64,7 +65,9 @@ def lowest_priority_operation(_list):
     else: 
         return -1
 
-
+"""
+Check if can get rid of parenthesis on the sides
+"""
 def can_strip_parenthesis(_list):
     if _list[0] == "(" and _list[-1] == ")":
         count_par = 0
@@ -81,9 +84,15 @@ def can_strip_parenthesis(_list):
                 
     else: return False
 
+"""
+If a list is bounded by parenthesis and can_strip_parenthesis, get rid of them
+"""
 def strip_parenthesis(_list):
     return _list[1:-1]
 
+"""
+Solve a simple operation
+"""
 def solve_op(op, left_op, right_op):
     if op == "+":
         result = left_op + right_op
@@ -100,41 +109,36 @@ def solve_op(op, left_op, right_op):
         result = left_op ** right_op
     return result
 
-
+"""
+Takes a tree as argument and start resolving from the left side
+"""
 def solve_tree(_root):
-    print("Operacion: ", _root.data, end=" ")
+    #Check if _root is a leaf node
     if _root.left == None and _root.right == None:
         return _root.data
+
+    #If left child is operation, solve it
     if _root.left.data in allowed_ops:
         left_op = solve_tree(_root.left)
-
-        print("Left: ", _root.left.data, end=" ")
-
     else:
         left_op = _root.left.data
 
-        print("Left: ", _root.left.data, end=" ")
-
+    #If left child is operation, solve it
     if _root.right.data in allowed_ops:
         right_op = solve_tree(_root.right)
-
-        print("right: ", _root.right.data, end=" ")
-
     else: 
         right_op = _root.right.data
 
-        print("right: ", _root.right.data)
-
+    #If any of the previos ops gives ZeroDivisionError
     if left_op == zerodivision or right_op == zerodivision:
         return zerodivision
     else:
         result = solve_op(_root.data, left_op, right_op)
         return result
 
-
-
-
-
+"""
+Print tree, for debug only
+"""
 def printNodes(node):
     print(node.data)
     if node.left != None:
@@ -142,6 +146,9 @@ def printNodes(node):
     if node.right != None:
         printNodes(node.right)
 
+"""
+Take a list as argument and checks if the parenthesis match
+"""
 def count_parenthesis(lista):
     count = 0
     for element in lista:
@@ -151,6 +158,9 @@ def count_parenthesis(lista):
             count -= 1
     return count
 
+"""
+Takes a list as argument and solve it
+"""
 def solve(lista):
     if count_parenthesis(lista):
         return "Cuenta mal formada, comprueba los parentesis"
@@ -158,27 +168,26 @@ def solve(lista):
     return solve_tree(root_node)
 
 """
-***************************************************
-
-Aqui añado lo nuevo de menú
-
-***************************************************
+Prints welcome menu
 """
-
-
 def print_menu():
     print("\n")
-    menu = """Bienvenido a la calculadora
+    menu = """***********************************
+*   Bienvenido a la calculadora   *
+***********************************
     \nLas operaciones permitidas son:
-    \tsuma("+")
-    \tresta("-")
-    \tmultiplicacion("*")
-    \tdivision ("/")
-    \texponenciales("**")
-    \traices cuadradas("sqrt")
-    Esta calculadora soporta operaciones compuestas y parentesis, ademas, respeta las prioridades de las operaciones"""
+    \tSuma("+")
+    \tResta("-")
+    \tMultiplicacion("*")
+    \tDivision ("/")
+    \tExponenciales("**")
+    \tRaices cuadradas("sqrt")
+    \nEsta calculadora soporta operaciones compuestas y parentesis, ademas, respeta las prioridades de las operaciones\n\n"""
     print(menu)
 
+"""
+Prints allowed ops on each moment
+"""
 def print_operations():
     cad="""Elija una operación:
     \t+. Suma
@@ -194,8 +203,9 @@ def print_operations():
         cad = cad + "\n\t=. Calcular"
     print(cad)
 
-
-
+"""
+Gets the input of a user and check if its a valid operation, then returns it
+"""
 def get_operator():
     while True:
         op = input("Introduce la operacion: ")
@@ -208,17 +218,20 @@ def get_operator():
         else:
             print("Operacion no valida")
 
+"""
+Gets the input of a user and check if its a valid number or ( or sqrt, then returns it
+"""
 def get_number():
     while True:
         try:
             if last_result != None and not in_sqrt:
-                cad = ("Introduce un numero, un simbolo de parentesis, sqrt para una raiz cuadrada o \"r\" para usar el resultado anterior: ")
+                cad = ("Introduce un numero, un simbolo de parentesis \"(\", \"sqrt\" para una raiz cuadrada o \"r\" para usar el resultado anterior: ")
             elif last_result != None and in_sqrt:
-                cad = ("Introduce un numero, un simbolo de parentesis o \"r\" para usar el resultado anterior: ")
+                cad = ("Introduce un numero, un simbolo de parentesis \"(\" o \"r\" para usar el resultado anterior: ")
             elif in_sqrt:
                 cad = ("Introduce un numero o un simbolo de parentesis: ")
             else:
-                cad = ("Introduce un numero o un simbolo de parentesis o sqrt para una raiz cuadrada: ")
+                cad = ("Introduce un numero o un simbolo de parentesis \"(\" o \"sqrt\" para una raiz cuadrada: ")
             num1 = input(cad)
             if num1 == "r" and last_result != None:
                 return last_result
@@ -233,6 +246,9 @@ def get_number():
         except ValueError:
             print("Numero invalido, introduce un numero (usa el punto \".\" como separacion decimal)")
 
+"""
+Takes the end list of the sqrt and generate a beautiful one and appends the ops
+"""
 def append_sqrt(radicando):
     global visual_op
     global regular_op
@@ -244,9 +260,12 @@ def append_sqrt(radicando):
     regular_op += radicando
     return radicando
 
+"""
+Gets the ops inside an sqrt
+"""
 def sqrt_maker():
     radicando = []
-    print("\n\nIntroduce el radicando de la raiz cuadrada (no se permites raices anidadas): \n")
+    print("\n\nIntroduce el radicando de la raiz cuadrada (no se permiten raices anidadas): \n")
 
     while True:
         while True:
@@ -282,25 +301,29 @@ def sqrt_maker():
                 print()
                 break
 
-    
 #Here starts the program
+#End variable as flag for main loop
+#last_result stores the result of the last operacion
+#in_sqrt flag to know that we are inside a sqrt operation
 end = False
 last_result = None
 in_sqrt = False
 
 print_menu()
 input("Para comenzar una operacion pulse cualquier tecla")
+
 while not end:
     print("\nNueva operacion")
     parenthesis_num = 0
     visual_op = []
     regular_op = []
 
-    
     while True:
-        
         #Add number, and if its a parenthesis, let the user add another number
         while True:
+            for i in visual_op:
+                print(i, end=" ")
+            print()
             num = get_number()
             if num == "sqrt":
                 in_sqrt = True
@@ -310,19 +333,19 @@ while not end:
             else:
                 visual_op.append(num)
                 regular_op.append(num)
-            print()
-            for i in visual_op:
-                print(i, end=" ")
-            print("")
+            
+            
             if num != "(":
                 break
             else:
                 parenthesis_num += 1
 
-
         #Add operation
         while True:
             print_operations()
+            for i in visual_op:
+                print(i, end=" ")
+            print()
             operation = get_operator()
             if operation == "=":
                 visual_op.append(operation)
@@ -338,20 +361,24 @@ while not end:
         if operation == "=":
             break
         print()
-        for i in visual_op:
+        """for i in visual_op:
             print(i, end=" ")
-        print()
+        print()"""
     for i in visual_op:
         print(i, end=" ")
+
+    #Once the user ends the input, calculate the result
     result = solve(regular_op)
-    print(result)
+    print()
+    print('\033[92m' ,result, '\033[0m')
+    print()
     if result != zerodivision:
         last_result = result
-    ##calcular el resultado aquí
+
     next = input("Si desea realizar otra operacion pulse s, de lo contrario pulse cualquier otra letra: ")
     if(next.lower() != "s"):
         end = True
 
 
-print("Gracias por usar mi calculadora, vuelva pronto!")
+print("\nGracias por usar mi calculadora, vuelva pronto!\n")
 
